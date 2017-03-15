ff2014e44657093261972eb9e8dc2c272b8e62e8
LamboCreeper <bl.purdue@sky.com>
2017-03-15 18:29:25 +0000

    Added Case Sensitivity based Arguments.

--- main.js
+++ main.js
@@ -14,4 +14,36 @@ client.on('ready', () => {
 	console.log(prefix + "Ready to go!");
 });
 
+client.on('message', message => {
+
+	// Make the input in capitals so for CaSeSENsiTivIty reasons.
+	msgSent = message.content.toUpperCase();
+
+	// About command - @MCStatus
+	if (msgSent == '<@291623138457026560>') {
+		message.channel.sendMessage(msg.about.p1 + message.author + msg.about.p2);
+	}
+
+	// Server status command - @MCStatus server
+	if (message.content.startsWith('<@291623138457026560>')) {
+
+		console.log('1');
+
+		var args = message.content.split(" ").slice(1);
+
+		console.log(args);
+
+		arg0 = args[0].toUpperCase();
+
+		if (arg0 == 'SERVER') {
+			if (typeof args[1] != 'undefined') {
+
+			} else {
+				message.channel.sendMessage(msg.server.not_defined);
+				message.channel.sendMessage(msg.server.usage);
+			}
+		}
+	}
+});
+
 client.login(auth.token);

a0205d66c47d2a5e532820d5ad17d3a2edff827f
LamboCreeper <bl.purdue@sky.com>
2017-03-18 14:32:47 +0000

    Can get JSON text from sites.

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const http = require('http');
-const nQuery = require('nodeQuery');
+var request = require("request");
 const msg = require('./messages.json');
 const auth = require('./auth.json');
 
@@ -23,7 +22,7 @@ client.on('message', message => {
 
 	// About command - @MCStatus
 	if (msgSent == '<@291623138457026560>') {
-		message.channel.sendMessage(msg.about.p1 + message.author + msg.about.p2);
+
 	}
 
 	// Server status command - @MCStatus server
@@ -31,26 +30,38 @@ client.on('message', message => {
 
 		// Creating arguments and making them not CaSeSENsiTivItE.
 		var args = message.content.split(" ").slice(1);
-		arg0 = args[0].toUpperCase();
 
-		if (arg0 == 'SERVER') {
-			if (typeof args[1] != 'undefined') {
-				console.log(args[1]);
-				var result;
-				var app = function ($) {
-					$.on('ready', function () {
-						$.get( "https://mcapi.us/server/status?ip=" + args[1], function( data ) {
-					  	 	result = data;
-						});
+		if (typeof args[0] != 'undefined') {
+			arg0 = args[0].toUpperCase();
+
+			if (arg0 == 'SERVER') {
+				if (typeof args[1] != 'undefined') {
+					console.log(args[1]);
+					// $.get( "https://mcapi.us/server/status?ip=" + args[1], function( data ) {
+					//   	 result = data;
+					// });
+
+					var url = "https://mcapi.us/server/status?ip=" + args[1]
+					request({
+					    url: url,
+					    json: true
+					}, function (error, response, body) {
+					    if (!error && response.statusCode === 200) {
+					        console.log(body) // Print the json response
+							var result = body;
+							console.log('-');
+							console.log(result);
+							message.channel.sendMessage(JSON.stringify(result));
+					    }
 					});
-				}
-				message.channel.sendMessage(result);
 
-				nQuery.use(app);
-			} else {
-				message.channel.sendMessage(msg.server.not_defined);
-				message.channel.sendMessage(msg.server.usage);
+				} else {
+					message.channel.sendMessage(msg.server.not_defined);
+					message.channel.sendMessage(msg.server.usage);
+				}
 			}
+		} else {
+			message.channel.sendMessage(msg.about.p1 + message.author + msg.about.p2);
 		}
 	}
 });

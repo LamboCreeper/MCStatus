9c3b883ac5ef52b5c2d50d227c1de51887197459
LamboCreeper <bl.purdue@sky.com>
2017-03-19 16:05:04 +0000

    Added Support for Offline Servers

--- main.js
+++ main.js
@@ -43,25 +43,41 @@ client.on('message', message => {
 					    json: true
 					}, function (error, response, body) {
 					    if (!error && response.statusCode === 200) {
-							const serverEmbded = new Discord.RichEmbed()
+							if (body.motd == '') {
+								const serverEmbded = new Discord.RichEmbed()
 
-							.setColor('#66A866')
-							var motd = body.motd;
-							var formattingCodes = ['§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§0', '§a', '§b', '§c', '§d', '§e', '§f', '§r', '§l', '§o', '§n', '§m', '§k']
-							for (code in formattingCodes) {
-								motd = motd.replace(formattingCodes[code], '');
+								.setColor('#66A866')
+								serverEmbded.setTitle(args[1])
+								serverEmbded.addField('Online', body.online)
+								serverEmbded.addField('Players Online', "0/?")
+								serverEmbded.addField('Game Version', "?")
+
+								serverEmbded.setFooter('MCStatus by LamboCreeper')
+								serverEmbded.setThumbnail('https://mcapi.ca/query/' + args[1] + '/icon')
+								serverEmbded.setURL('http://lcurl.xyz/MCStatusBot')
+
+								message.channel.sendEmbed(serverEmbded, '', { disableEveryone: true });
+							} else {
+								const serverEmbded = new Discord.RichEmbed()
+
+								.setColor('#66A866')
+								var motd = body.motd;
+								var formattingCodes = ['§1', '§2', '§3', '§4', '§5', '§6', '§7', '§8', '§9', '§0', '§a', '§b', '§c', '§d', '§e', '§f', '§r', '§l', '§o', '§n', '§m', '§k']
+								for (code in formattingCodes) {
+									motd = motd.replace(formattingCodes[code], '');
+								}
+								serverEmbded.setTitle(motd)
+								serverEmbded.addField('Online', body.online)
+								serverEmbded.addField('Players Online', body.players.now + "/" + body.players.max)
+								serverEmbded.addField('Game Version', body.server.name)
+
+								serverEmbded.setFooter('MCStatus by LamboCreeper')
+								serverEmbded.setThumbnail('https://mcapi.ca/query/' + args[1] + '/icon')
+								serverEmbded.setURL('http://lcurl.xyz/MCStatusBot')
+
+								message.channel.sendEmbed(serverEmbded, '', { disableEveryone: true });
 							}
-							serverEmbded.setTitle(motd)
-							serverEmbded.addField('Online', body.online)
-							serverEmbded.addField('Players Online', body.players.now + "/" + body.players.max)
-							serverEmbded.addField('Game Version', body.server.name)
-
-							serverEmbded.setFooter('MCStatus by LamboCreeper')
-							serverEmbded.setThumbnail('https://mcapi.ca/query/' + args[1] + '/icon')
-							serverEmbded.setURL('http://lcurl.xyz/MCStatusBot')
-
-							message.channel.sendEmbed(serverEmbded, '', { disableEveryone: true });
-					    }
+						}
 					});
 
 				} else {
